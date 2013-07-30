5dd074b5208c334438289a3d6ade8276d1fbd1bc
scaledsoftware@gmail.com <tim.v2.0@gmail.com>
2013-07-30 14:26:01 -0700

    Can now create linkage between funds

--- Handler/CreateFund.hs
+++ Handler/CreateFund.hs
@@ -3,23 +3,27 @@ module Handler.CreateFund where
 import Import
 
 postCreateFundR :: PersonId -> Handler Html
-postCreateFundR fundPId = do
+postCreateFundR otherPId = do
     Entity _ appUser <- requireAuth
     let userPId = appUserPerson appUser
 
-    if fundPId == userPId
+    if otherPId == userPId
       then do
           setMessageI MsgCannotCreateFundWSelf
           redirect FundsR
       else do
-          currTime <- liftIO getCurrentTime
-          fundP <- runDB $ get404 fundPId
-          let fund = Fund (if userPId < fundPId then userPId else fundPId)  --p1
-                          (if userPId > fundPId then userPId else fundPId)  --p2
-                          (personNickName fundP)
-                          currTime
-
-          fundId <- runDB $ insert fund
-          redirect $ FundR fundId
-          
+          maybeFund <- runDB $ getBy $ UniqueFund (min userPId otherPId) (max userPId otherPId)
+          case maybeFund of
+              Just (Entity fundId _) -> redirect $ FundR fundId
+              Nothing -> do
+                  currTime <- liftIO getCurrentTime        
+                  fundP <- runDB $ get404 otherPId        
+                  let fund = Fund (min userPId otherPId)    -- p1        
+                                  (max userPId otherPId)    -- p2        
+                                  (personNickName fundP)    -- nickname        
+                                  currTime                  -- created time        
+                
+                  fundId <- runDB $ insert fund        
+                  redirect $ FundR fundId        
+                  
 
