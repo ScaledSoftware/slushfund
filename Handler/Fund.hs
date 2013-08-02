18f429d31fc04e0dc2b289b54fd0e738f9a40dba
scaledsoftware@gmail.com <tim.v2.0@gmail.com>
2013-08-02 12:03:18 -0700

    Now displaying fund entry contents

--- Handler/Fund.hs
+++ Handler/Fund.hs
@@ -2,14 +2,23 @@ module Handler.Fund where
 
 import Import
 
-getFundR :: FundId -> Handler Html
-getFundR pid = do
-    defaultLayout $ do
-        setTitleI $ MsgFundTitle $ " FIXME"
-        $(widgetFile "fund")
 
-postFundR :: FundId -> Handler Html
-postFundR pid = do
+getFundR :: FundId -> Handler Html
+getFundR fundId = do
+    fund <- runDB $ get404 fundId
+    let fundNick = fundNickName fund
+    fundEntries <- runDB $ selectList [FundEntryFund ==. fundId] [Asc FundEntryTransDate]
+
+
+    Entity _ appUser <- requireAuth
+    let myPId = appUserPerson appUser
+    me <- runDB $ get404 $ myPId
+    let theirPId = fundOtherPerson (appUserPerson appUser) fund
+    them <- runDB $ get404 $ theirPId
+
+    let decoratedFundEntries = map (\(Entity feId fe) -> 
+          (feId, fe, if myPId == fundEntryPayer fe then me else them)) fundEntries
+
     defaultLayout $ do
-        setTitleI $ MsgFundTitle $ " FIXME"
+        setTitleI $ MsgFundTitle $ fundNick
         $(widgetFile "fund")
