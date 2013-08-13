c502fa69bcab8f579b1dd189cd5b639ed170e923
scaledsoftware@gmail.com <tim.v2.0@gmail.com>
2013-08-12 20:09:00 -0700

    added content to home page

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -3,6 +3,8 @@ module Handler.Home where
 
 import Import
 import Yesod.Auth
+import Data.Maybe
+import Control.Monad.Trans.Maybe
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -13,9 +15,26 @@ import Yesod.Auth
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    maid <- maybeAuthId
+    mAuth <- maybeAuth
+
+    mFunds <- runMaybeT $ do
+        entityAuth <- MaybeT $ maybeAuth
+        let myPId = appUserPerson $ entityVal entityAuth
+        entityFunds <- lift $ runDB $ selectList ([FundP1 ==. myPId] ||. [FundP2 ==. myPId])
+                                          [Asc FundCreated]
+        let funds = map (\(Entity fId f) -> (fId, f)) entityFunds
+        let pid2Fund = map (\f -> (fundOtherPerson myPId $ snd f, fst f)) funds
+        entityPersons <- lift $ runDB $ selectList [PersonId <-. (map fst pid2Fund)]
+                                            [Asc PersonNickName]
+        let nickname2fid = map (\(Entity pid person) ->
+                            (personNickName person, fromJust $ lookup pid pid2Fund)) entityPersons
+
+        return nickname2fid
+
+    entries <- runDB $ selectList [] [Asc EntryCreated, LimitTo 1]
+
+
     defaultLayout $ do
         aDomId <- newIdent
         setTitleI MsgWelcomeHomepage
-        $(widgetFile "homepage")
-
+        $(widgetFile "homepage") 
